c25751a44275322ace46ce77f56507ceaf338097
Lakshna S <78756909+Royely08@users.noreply.github.com>
2021-10-18 20:32:20 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 
 img = "";
 status = "";
+objects = [];
 
 function preload(){
     img = loadImage('dog_cat.jpg');
@@ -15,6 +16,20 @@ function setup(){
 
 function draw(){
     image(img,0,0,640,420);
+
+    if(status != ""){
+        for(i = 0; i<objects.length; i++){
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+
+            fill("#ff0000");
+            percent = floor(objects[i].confidence*100);
+            text(objects[i].label + " " + percent + "%",objects[i].x,objects[i].y);
+            noFill();
+            stroke("#ff0000");
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+        }
+    }
+  /*  image(img,0,0,640,420);
     fill("#ff0000");
     text("Dog",45,75);
     noFill();
@@ -25,7 +40,7 @@ function draw(){
     text("Cat",320,120);
     noFill();
     stroke("#ff0000");
-    rect(300,90,270,320);
+    rect(300,90,270,320); */
 }
 
 function modelLoaded(){
@@ -34,11 +49,12 @@ function modelLoaded(){
     objectDetector.detect(img,gotResult);
 }
 
-function gotResult(){
+function gotResult(error,results){
     if(error){
         console.log(error);
     }
     else{
         console.log(results);
+        objects = results;
     }
 }
